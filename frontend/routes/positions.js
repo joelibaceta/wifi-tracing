27ffec5bfd61fc2372b8c48de1eb8e7c909c49a8
Joel Ibaceta <mail@joelibaceta.com>
2020-07-26 04:21:03 -0500

    get points

--- frontend/routes/positions.js
+++ frontend/routes/positions.js
@@ -5,7 +5,12 @@ router.get('/save', function(req, res, next) {
 
   points = req.body.points;
 
-  
+  i1 = points[0]["x"]
+  i2 = points[1]["x"]
+  i3 = points[2]["x"]
+  j1 = points[0]["y"]
+  j2 = points[1]["y"]
+  j3 = points[2]["y"]
 
   res.send('respond with a resource');
 });
