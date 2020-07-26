d3671ea36e4430dc3bcd482be6828b770cbc9bf9
Yahyr Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2020-07-26 09:04:14 -0500

    fix point green for people

--- frontend/routes/users.js
+++ frontend/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  const HARD_CODED_ROUTER_POSITIONS =  {"x": 7, "y": 4};
+
+  res.json( HARD_CODED_ROUTER_POSITIONS);
+
 });
 
 module.exports = router;
