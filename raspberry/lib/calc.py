1b60549f4b131fa6f097dbbf8d13012550ecf55c
Joel Ibaceta <mail@joelibaceta.com>
2020-07-26 03:21:17 -0500

    using abs Ã

--- raspberry/lib/calc.py
+++ raspberry/lib/calc.py
@@ -4,7 +4,7 @@ def dbm2m(frequency, dBm):
 
   FSPL = 27.55 #Free-Space Path Loss
 
-  m = 10 ** (( FSPL - (20 * log10(frequency)) + dBm ) / 20 )
+  m = 10 ** (( FSPL - (20 * log10(frequency)) + abs(dBm) ) / 20 )
 
   return round(m, 2)
    
