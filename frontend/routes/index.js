721310dc912e99ad3badcaed1b7353e66f580225
Yahyr Paredes Arteaga <yahyrparedesarteaga@gmail.com>
2020-07-26 04:17:32 -0500

    point fix

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -8,6 +8,12 @@ router.get('/', function(req, res, next) {
   let points = [
     { translate: '94 88', color: '#f60600', name: '1'},
     { translate: '385 338', color: '#f60600', name: '2'},
+    { translate: '500 200', color: '#f60600', name: '3'},
+    { translate: '100 330', color: '#f60600', name: '3'},
+    { translate: '200 330', color: '#f60600', name: '3'},
+    { translate: '300 330', color: '#f60600', name: '3'},
+    { translate: '400 330', color: '#f60600', name: '3'},
+    { translate: '500 330', color: '#f60600', name: '3'},
     { translate: '708 330', color: '#f60600', name: '3'}
   ];
 
